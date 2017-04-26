02dfb5e7f7d17e2ddf87e28c3822e578efdae9cc
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-26 18:16:59 +0600

    Изменен вывод новостей

--- resources/views/front/posts/index.blade.php
+++ resources/views/front/posts/index.blade.php
@@ -12,34 +12,43 @@
 
         <h1>Новости портала</h1>
 
-        @for($i = 0; $i < count($model->posts);$i++)
+        <div class="uk-child-width-1-3@m uk-grid-medium uk-grid-match" uk-grid>
 
-            @php
-                /** @var \App\Models\Post $post */
-                $post = $model->posts[$i];
-                $url = url('news/'.$post->id)
-            @endphp
+            @for($i = 0; $i < count($model->posts);$i++)
 
-            <div class="habb-post">
-                <div class="">
-                    <a href="{{ $url }}" class="h2 uk-button uk-button-text"># {{ $post->title }}</a>
-                </div>
+                @php
+                    /** @var \App\Models\Post $post */
+                    $post = $model->posts[$i];
+                    $url = url('news/'.$post->id)
+                @endphp
 
-                <div class="uk-margin">
-                    {!! $post->getContentShortly(400) !!}
-                </div>
-
-
-                <div>
-
-                    <i class="fa fa-eye" aria-hidden="true"></i> {{ $post->views }}
-                    <div class="uk-float-right">
-                        <a href="{{ $url }}" class="uk-button uk-button-text">Подробнее</a>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body">
+                    <div class="uk-card-title uk-text-left">
+                        <a href="{{ $url }}" class="h2 uk-button uk-button-text uk-text-left">#{{ $post->title }}</a>
                     </div>
+                    <div class="uk-flex uk-flex-between">
+                        <div>
+                            <i class="fa fa-eye" aria-hidden="true"></i> {{ $post->views }}
+                        </div>
+                        <div>
+                            01.01.12
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        {!! $post->getContentShortly(400) !!}
+                    </div>
+
                 </div>
-                <hr>
             </div>
-        @endfor
+
+            @endfor
+
+
+        </div>
+
+
+
     </div>
 
 @endsection
