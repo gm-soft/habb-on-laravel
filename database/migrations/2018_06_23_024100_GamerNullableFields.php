0bbae71be78d7a0d78330fc4584b54474feceb2f
Maxim Gorbatyuk <maximgorbatyuk191093@gmail.com>
2018-06-23 09:17:43 +0600

    Начал вести разработку по ApiController для сторонних сервисов, добавил миграции для изменения таблиц

---
+++ database/migrations/2018_06_23_024100_GamerNullableFields.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GamerNullableFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('gamers', function (Blueprint $table) {
+
+            $table
+                ->dateTime('birthday')
+                ->nullable()
+                ->comment("День рождения человека")
+                -> change();
+
+            $table
+                ->string('city')
+                ->nullable()
+                ->comment("Город, указанный при регистрации")
+                -> change();
+
+            $table
+                ->string('vk_page')
+                ->nullable()
+                ->comment("Ссылка на профиль Вконтакте")
+                -> change();
+
+            $table
+                ->string('status')
+                ->nullable()
+                ->comment("Ученик/Студент/Работает/Тунеядец")
+                -> change();
+
+            $table
+                ->string('institution')
+                ->nullable()
+                ->comment("Место, где занят человек")
+                -> change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gamers', function (Blueprint $table) {
+            $table
+                ->dateTime('birthday')
+                ->nullable(false)
+                ->comment("День рождения человека")
+                -> change();
+
+            $table
+                ->string('city')
+                ->nullable(false)
+                ->comment("Город, указанный при регистрации")
+                -> change();
+
+            $table
+                ->string('vk_page')
+                ->nullable(false)
+                ->comment("Ссылка на профиль Вконтакте")
+                -> change();
+
+            $table
+                ->string('status')
+                ->nullable(false)
+                ->comment("Ученик/Студент/Работает/Тунеядец")
+                -> change();
+
+            $table
+                ->string('institution')
+                ->nullable(false)
+                ->comment("Место, где занят человек")
+                -> change();
+        });
+    }
+}
