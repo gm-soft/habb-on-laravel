8f8c99ab8a6e0a5397d689d9d583f8043f931381
maximgorbatyuk <maxim.gorbatyuk@outlook.com>
2018-11-25 14:54:27 +0600

    Minor fix

--- resources/views/admin/teams/show.blade.php
+++ resources/views/admin/teams/show.blade.php
@@ -11,25 +11,55 @@
 
 
         <div class="mt-2">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-text">
-                        <dl class="row">
-                            <dt class="col-sm-4">Город</dt>         <dd class="col-sm-8">{{ $team->city }}</dd>
-                            <dt class="col-sm-4">Комментарий</dt>   <dd class="col-sm-8">{{ $team->comment }}</dd>
-                        </dl>
-                    </div>
 
-                </div>
-                <div class="card-footer">
-                    {{ link_to_action('TeamController@index', 'В список', [], ['class' => 'btn btn-light']) }}
-                    <div class="float-sm-right">
+            <div class="row">
+                <div class="col-md-6 h-100">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-text">
+                                <dl class="row">
+                                    <dt class="col-sm-4">Город</dt>         <dd class="col-sm-8">{{ $team->city }}</dd>
+                                    <dt class="col-sm-4">Комментарий</dt>   <dd class="col-sm-8">{{ $team->comment }}</dd>
+                                </dl>
+                            </div>
 
-                        {{ link_to_action('TeamController@edit', 'Редактировать', ['id' => $team->id], ['class' => 'btn btn-primary']) }}
-                        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteDialog">Удалить</button>
+                        </div>
+                        <div class="card-footer">
+                            {{ link_to_action('TeamController@index', 'В список', [], ['class' => 'btn btn-light']) }}
+                            <div class="float-sm-right">
+
+                                {{ link_to_action('TeamController@edit', 'Редактировать', ['id' => $team->id], ['class' => 'btn btn-primary']) }}
+                                <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#deleteDialog">Удалить</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                <div class="col-md-6 h-100">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-text">
+                                <div class="h3">Турниры, где участвует</div>
+                                @php
+                                    $tournaments = $team->tournaments
+                                @endphp
+
+                                <ul>
+                                @foreach($tournaments as $tournament)
+                                    <li>
+                                        <a href="{{ action('TournamentsController@show', ['id' => $tournament->id]) }}">{{ $tournament->name }}</a>
+                                    </li>
+                                @endforeach
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
+
+
 
         </div>
 
