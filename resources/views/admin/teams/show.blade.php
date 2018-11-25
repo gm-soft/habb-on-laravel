4a38c0215e869e8e6dba4f56be684a30cba61a77
maximgorbatyuk <maxim.gorbatyuk@outlook.com>
2018-11-25 14:56:17 +0600

    +

--- resources/views/admin/teams/show.blade.php
+++ resources/views/admin/teams/show.blade.php
@@ -41,7 +41,7 @@
                             <div class="card-text">
                                 <div class="h3">Турниры, где участвует</div>
                                 @php
-                                    $tournaments = $team->tournaments
+                                    $tournaments = $team->tournamentsThatTakePart
                                 @endphp
 
                                 <ul>
